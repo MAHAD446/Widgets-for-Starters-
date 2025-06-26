9dc834ada5245644de3455eff5f027095d0dc9a5
mahad446 <mahadmubashir43@gmail.com>
2025-06-26 14:48:17 -0700

    activity#1.py

---
+++ activity#1.py
@@ -0,0 +1,10 @@
+# import necessary libraries
+from tkinter import *
+
+# creat window
+window = Tk()
+
+#set the window Titel and Geometry
+window.title('Demo Window')
+window.geometry('400x300')
+window.mainloop()
